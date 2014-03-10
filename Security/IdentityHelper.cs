ee761ed0f973fda48a5e27756b4a07843146a983
Chris Doty <dotyc@civicinc.com>
2014-03-10 13:03:03 -0500

    add identitymanager to core

---
+++ Security/IdentityHelper.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+
+namespace Civic.Core.Framework.Security
+{
+    public static class IdentityManager
+    {
+        public static string Username
+        {
+            get
+            {
+                return "Unknown";
+            }
+        }
+
+        public static string ClientMachine
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(HttpContext.Current.Request.UserHostName))
+                    return HttpContext.Current.Request.UserHostName;
+                return HttpContext.Current.Request.UserHostAddress;
+            }
+        }  
+    }
+}
