09aa26255092eab99451a0281a323fcdcb89bfd5
Chris Doty <dotyc@civicinc.com>
2014-03-10 23:41:34 -0500

    clean up some audit manager functionality

--- Security/IdentityHelper.cs
+++ Security/IdentityHelper.cs
@@ -18,7 +18,19 @@ namespace Civic.Core.Framework.Security
             {
                 if (!string.IsNullOrEmpty(HttpContext.Current.Request.UserHostName))
                     return HttpContext.Current.Request.UserHostName;
-                return HttpContext.Current.Request.UserHostAddress;
+             
+                if (!string.IsNullOrEmpty(HttpContext.Current.Request.UserHostAddress))
+                    return HttpContext.Current.Request.UserHostAddress;
+
+                // TODO: THIS NEEDS TO BE OPTIONALLY TURNED ON
+                foreach (var key in HttpContext.Current.Request.Headers.AllKeys)
+                {
+                    if (key.ToLowerInvariant() == "x-forwarded-for")
+                    {
+                        return HttpContext.Current.Request.Headers[key];
+                    }
+                }
+                return "Unknown";
             }
         }  
     }
