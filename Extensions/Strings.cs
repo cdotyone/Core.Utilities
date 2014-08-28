dc2c06c2b78ca2c470f4ceee95418cacaf0d8104
Chris Doty <dotyc@civicinc.com>
2014-08-28 15:56:41 -0500

    method not public

--- Extensions/Strings.cs
+++ Extensions/Strings.cs
@@ -2,7 +2,7 @@
 {
     public static class Strings
     {
-        internal static string Truncate(this string instr, int maxLength)
+        public static string Truncate(this string instr, int maxLength)
         {
             if (maxLength > instr.Length) maxLength = instr.Length;
             return instr.Substring(0, maxLength);
