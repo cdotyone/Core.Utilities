d0823f4d4ef89493ba9b94402bdff00400d9caaf
Chris Doty <dotyc@civicinc.com>
2014-08-28 15:48:30 -0500

    add generic string extension class

---
+++ Extensions/Strings.cs
@@ -0,0 +1,11 @@
+﻿namespace Civic.Core.Framework.Extensions
+{
+    public static class Strings
+    {
+        internal static string Truncate(this string instr, int maxLength)
+        {
+            if (maxLength > instr.Length) maxLength = instr.Length;
+            return instr.Substring(0, maxLength);
+        }
+    }
+}
