28f222791fa21e238124cf726b53d0e97536e63a
Chris Doty <dotyc@civicinc.com>
2017-02-10 13:36:37 -0600

    ...

--- Civic.Core.Framework/Extensions/DateTimeExtensions.cs
+++ Civic.Core.Framework/Extensions/DateTimeExtensions.cs
@@ -8,13 +8,13 @@ namespace Civic.Core.Framework.Extensions
 {
     public static class DateTimeExtensions
     {
-        public static DateTime? StartOfDay(DateTime?  value)
+        public static DateTime? StartOfDay(this DateTime?  value)
         {
             if (value.HasValue) return DateTime.Parse(value.Value.ToShortDateString());
             return null;
         }
 
-        public static DateTime? EndofDay(DateTime? value)
+        public static DateTime? EndofDay(this DateTime? value)
         {
             if (value.HasValue) return DateTime.Parse(value.Value.ToShortDateString()).AddDays(1).AddMilliseconds(-1);
             return null;
