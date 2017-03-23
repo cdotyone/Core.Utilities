040c89b4855f5a71586ea33ca8237e90b4dca06b
Chris Doty <dotyc@civicinc.com>
2017-03-23 16:44:26 -0500

    and angular redirect handler

---
+++ Civic.Core.Framework/Web/Handlers/AngularLinkHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Civic.Core.Framework.Web.Handlers
+{
+    public class AngularLinkHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var uri = context.Request.Url;
+
+            var angularPath = uri.PathAndQuery;
+            angularPath = angularPath.Substring(context.Request.ApplicationPath.Length);
+
+            var host = uri.AbsoluteUri.Replace(uri.PathAndQuery, "");
+            if (context.Request.ApplicationPath != "/" && context.Request.ApplicationPath != "")
+                host += context.Request.ApplicationPath;
+
+            context.Response.Redirect(host + "/#" + angularPath);
+        }
+
+        public bool IsReusable
+        {
+            get { return true; }
+        }
+    }
+}
