d9c580d144ab57662a1ca977f1f39ab67cdac413
Tanner Terry <terryt@civicinc.com>
2017-12-15 09:51:11 -0600

    Build(gitlab) - Refactor

---
+++ References/VersionInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("1.24.0.0")]
+[assembly: AssemblyFileVersion("1.24.0.0")]
+[assembly: AssemblyCompany("Civic Engineering & IT")]
+[assembly: AssemblyCopyright("Copyright © 2017 Civic Engineering & IT")]
