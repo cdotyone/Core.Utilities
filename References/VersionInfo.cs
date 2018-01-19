ef4d1c218160293084064441e86ceff20652b445
Chris Doty <dotyc@civicinc.com>
2018-01-18 22:29:16 -0600

    bump year

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -3,4 +3,4 @@
 [assembly: AssemblyVersion("1.24.0.0")]
 [assembly: AssemblyFileVersion("1.24.0.0")]
 [assembly: AssemblyCompany("Civic Engineering & IT")]
-[assembly: AssemblyCopyright("Copyright © 2017 Civic Engineering & IT")]
+[assembly: AssemblyCopyright("Copyright © 2018 Civic Engineering & IT")]
