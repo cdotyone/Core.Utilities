3197c6fec97c5373365ce6f30b5a8c72787333df
Chris Doty <dotyc@civicinc.com>
2020-07-22 22:57:09 -0500

    fix: packaging

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.2.160")]
 [assembly: AssemblyCompany("Chris Doty")]
-[assembly: AssemblyCopyright("Copyright © 2019 Chris Doty")]
+[assembly: AssemblyCopyright("Copyright © 2020 Chris Doty")]
