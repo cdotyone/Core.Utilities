a8b3316e134f419d6f5f3f4e7dd407132e0fd049
Chris Doty <dotyc@civicinc.com>
2020-08-04 20:48:19 -0500

    feat: add default extensions

---
+++ Core.Utilities/Extensions/DefaultExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Extensions
+{
+    public static class DefaultExtensions
+    {
+
+        [DebuggerStepThrough]
+        public static string Default(this string instr, string value)
+        {
+            if (instr.IsNullOrEmpty()) return value;
+            return instr;
+        }
+
+        [DebuggerStepThrough]
+        public static long Default(this long? inValue, long value)
+        {
+            if (!inValue.HasValue) return value;
+            return inValue.Value;
+        }
+
+        [DebuggerStepThrough]
+        public static long Default(this long inValue, long value)
+        {
+            if (inValue == 0) return value;
+            return inValue;
+        }
+
+        [DebuggerStepThrough]
+        public static int Default(this int? inValue, int value)
+        {
+            if (!inValue.HasValue) return value;
+            return inValue.Value;
+        }
+
+        [DebuggerStepThrough]
+        public static bool Default(this bool? inValue, bool value)
+        {
+            if (!inValue.HasValue) return value;
+            return inValue.Value;
+        }
+
+        [DebuggerStepThrough]
+        public static int Default(this int inValue, int value)
+        {
+            if (inValue==0) return value;
+            return inValue;
+        }
+    }
+}
