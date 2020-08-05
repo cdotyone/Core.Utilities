4a3c3b0feb90f0497216e0d26a2f30e796a231ae
Chris Doty <dotyc@civicinc.com>
2020-08-04 20:21:35 -0500

    fix: package version

--- References/VersionInfo.cs
+++ References/VersionInfo.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.2.160")]
+[assembly: AssemblyFileVersion("2.0.11.160")]
 [assembly: AssemblyCompany("Chris Doty")]
 [assembly: AssemblyCopyright("Copyright © 2020 Chris Doty")]
